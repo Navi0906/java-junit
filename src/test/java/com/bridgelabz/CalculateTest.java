ffce1324733c1add8c8d92655dd78f8a7cffed3b
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-04-03 09:39:22 +0530

    Naam Simran Singh Gill Add: Java junit

--- src/test/java/com/bridgelabz/CalculateTest.java
+++ src/test/java/com/bridgelabz/CalculateTest.java
@@ -22,5 +22,42 @@ public class CalculateTest {
         assertEquals(6, calculator.add(2,4));
     }
 
+    @Test
+    public void twoMinusFour(){
+        Calculator calculator = new Calculator();
+        assertEquals(-2, calculator.subtract(2,4));
+    }
+
+    @Test
+    public void twoMinusTwo(){
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.subtract(2,2));
+    }
+
+    @Test
+    public void twoMutiplyFour(){
+        Calculator calculator = new Calculator();
+        assertEquals(8, calculator.multiply(2,4));
+    }
+
+    @Test
+    public void twoMultiplytwo(){
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.multiply(2,2));
+    }
+
+    @Test
+    public void fourDivideTwo(){
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(4,2));
+    }
+
+    @Test
+    public void tenDivideZero(){
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class,
+                () -> {calculator.divide(10,0);});
+
+    }
 
 }
